ac3e412d4af4de0841496a00e838a92b6a8d57bf
devAgam <46868711+tippytactics@users.noreply.github.com>
2020-09-21 12:47:18 +0530

    Updates

---
+++ precache-manifest.2c12711cd4855567bd621adad47fde88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30473036582abe3b479c7b8d0e031f81",
+    "url": "/devAgam-MemeMaker/index.html"
+  },
+  {
+    "revision": "89462a3dbd09cc71e879",
+    "url": "/devAgam-MemeMaker/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "1d58966b14533fe42f12",
+    "url": "/devAgam-MemeMaker/static/js/2.a5cb9933.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/devAgam-MemeMaker/static/js/2.a5cb9933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89462a3dbd09cc71e879",
+    "url": "/devAgam-MemeMaker/static/js/main.8633870a.chunk.js"
+  },
+  {
+    "revision": "428a3b9bcb2544cb74d9",
+    "url": "/devAgam-MemeMaker/static/js/runtime-main.a8d35285.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/devAgam-MemeMaker/precache-manifest.d006c016babfa1345995150c958f6b59.js"
+  "/devAgam-MemeMaker/precache-manifest.2c12711cd4855567bd621adad47fde88.js"
 );
 
 self.addEventListener('message', (event) => {
