66e3928d23d458fda0dc8bcd6621080d1afc9a40
devAgam <46868711+tippytactics@users.noreply.github.com>
2020-09-21 12:38:20 +0530

    Updates

---
+++ precache-manifest.d006c016babfa1345995150c958f6b59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f5070c2799c7012b8bef918ab462a5",
+    "url": "/devAgam-MemeMaker/index.html"
+  },
+  {
+    "revision": "b763ae9d953770f52237",
+    "url": "/devAgam-MemeMaker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8fe7e7e4d22ece3bd79a",
+    "url": "/devAgam-MemeMaker/static/js/2.4a0b5433.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/devAgam-MemeMaker/static/js/2.4a0b5433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b763ae9d953770f52237",
+    "url": "/devAgam-MemeMaker/static/js/main.ae106231.chunk.js"
+  },
+  {
+    "revision": "428a3b9bcb2544cb74d9",
+    "url": "/devAgam-MemeMaker/static/js/runtime-main.a8d35285.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/devAgam-MemeMaker/static/media/logo.5d5d9eef.svg"
+  }
+]);
